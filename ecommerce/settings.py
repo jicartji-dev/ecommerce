228e47daf613de0f790d9847163702bdf0f6e2e2
AdarshFusion <chauhanadarsh432@gmail.com>
2025-12-31 23:37:04 +0530

    Initial Django ecommerce project

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,15 @@ ALLOWED_HOSTS = ["ecommerce-vjkj.onrender.com",".onrender.com",]
 #     "localhost",
 # ]
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+
+
 # DATABASE = PostgreSQL (Render)
 
 # Application definition
