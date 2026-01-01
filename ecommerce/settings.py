9cb9a7bd04f0687657ca03c8b6189b09a3a42d2a
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-01 21:30:58 +0530

    Added cloudinary for media storage

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -156,7 +156,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
