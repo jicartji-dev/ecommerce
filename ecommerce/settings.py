5fdb5277af1c7008efb41cede72a77a68c31d441
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-01 22:36:06 +0530

    Add static files for production

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -152,10 +152,18 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 
 
