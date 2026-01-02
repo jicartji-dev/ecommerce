d646ff81bec995c64f856a8ef1d50a395a285fab
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-02 19:00:41 +0530

    Add static files for production

--- cartji/urls.py
+++ cartji/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('', include('cartjiapp.urls')),
 ]
 
 if settings.DEBUG:
