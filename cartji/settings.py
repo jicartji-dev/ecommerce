cd9577be851ad99c4ce37a5b880a7d1879653376
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-02 19:55:50 +0530

    Add static files for production

--- cartji/settings.py
+++ cartji/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cartjiapp','cartjiapp.apps.CartjiappConfig',
+    'cartjiapp.apps.CartjiappConfig',
 
     
 ]
