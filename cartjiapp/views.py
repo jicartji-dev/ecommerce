24d911e77dfa33da7766baf724945419630a873e
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-02 23:08:09 +0530

    Add static files for production

--- cartjiapp/views.py
+++ cartjiapp/views.py
@@ -54,3 +54,8 @@ def about(request):
 
 def contact(request):
     return render(request, 'cartjiapp/contact.html')
+
+def nav_categories(request):
+    return {
+        "nav_categories": Category.objects.all()
+    }
