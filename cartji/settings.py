851a4b5f6b7c2ce8e36c224e44b1056f18c35994
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-02 20:22:24 +0530

    Add static files for production

--- cartji/settings.py
+++ cartji/settings.py
@@ -90,14 +90,22 @@ TEMPLATES = [
 ]
 
 
-# --------------------------------------------------
-# DATABASE (Render PostgreSQL)
-# --------------------------------------------------
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
+if os.environ.get("RENDER") == "true":
+    # Render production (PostgreSQL only)
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ.get("DATABASE_URL")
+        )
+    }
+else:
+    # Local development (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # --------------------------------------------------
