02e515962bbfbb656c703ec7708d508f9c9b217e
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-04 10:23:31 +0530

    Add static files for production

--- cartjiapp/context_processors.py
+++ cartjiapp/context_processors.py
@@ -2,8 +2,7 @@ from .models import Category, SubCategory
 
 def nav_categories(request):
     return {
-        "nav_categories": Category.objects.all(),
-        
+        "nav_categories": Category.objects.prefetch_related("subcategories"),        
         "nav_subcategories": SubCategory.objects.all(),
 
 
