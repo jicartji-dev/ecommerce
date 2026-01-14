76748d7ab9a112140f5afbd74ae8aba3b83036e6
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-14 22:04:05 +0530

    ready

--- cartji/settings.py
+++ cartji/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'cartji.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # DATABASES = {
@@ -93,13 +93,13 @@ WSGI_APPLICATION = 'cartji.wsgi.application'
 # }
 
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+# DATABASES = {
+#     "default": dj_database_url.parse(
+#         os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
 
 
 # Password validation
