eeeff73e31a477532545036c3dda4d20b82ee4b1
AdarshFusion <chauhanadarsh432@gmail.com>
2026-01-21 21:23:42 +0530

    css change

--- cartji/settings.py
+++ cartji/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6@v+*(9hy654xie=^xx5$t_2bq(kz#2h=6($9vhi(q99$es&9%'
+# SECRET_KEY = 'django-insecure-6@v+*(9hy654xie=^xx5$t_2bq(kz#2h=6($9vhi(q99$es&9%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
