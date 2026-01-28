81ed60816c675d812326145ef725c80881a58614
jicartji-dev <jicartji@gmail.com>
2026-01-28 19:07:01 +0530

    Update urls.py

--- cartji/urls.py
+++ cartji/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.urls import path, include, re_path
+from django.views.static import serve
 
-from django.urls import path, include
 
 from django.contrib.sitemaps.views import sitemap
 from cartjiapp.sitemaps import StaticSitemap
