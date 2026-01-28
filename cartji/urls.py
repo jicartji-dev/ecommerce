30f7c717a48f523324edea9073cbccde9bebba21
jicartji-dev <jicartji@gmail.com>
2026-01-28 18:57:35 +0530

    Update urls.py

--- cartji/urls.py
+++ cartji/urls.py
@@ -3,9 +3,16 @@ from django.contrib.auth import views as auth_views
 
 from django.urls import path, include
 
+from django.contrib.sitemaps.views import sitemap
+from cartjiapp.sitemaps import StaticSitemap
+sitemaps = {
+    "static": StaticSitemap,
+}
+
 urlpatterns = [
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
     path('admin/', admin.site.urls),
     path('', include('cartjiapp.urls')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    re_path(r"^(?P<path>.*)$
 ]
