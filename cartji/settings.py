4c9cc499e9e5adf4ddc41c4f09b548079f30500b
AdarshFusion <chauhanadarsh432@gmail.com>
2026-04-13 11:36:53 +0530

    some changess

--- cartji/settings.py
+++ cartji/settings.py
@@ -90,18 +90,15 @@ WSGI_APPLICATION = 'cartji.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=os.environ.get("DATABASE_URL",f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
-        ssl_require=True,
+        # ssl_require=True,
     )
 }
-
-
-DATABASES['default']['OPTIONS'] = {
-    'sslmode': 'require',
-}
-
-
+if 'postgresql' in DATABASES['default']['ENGINE']:
+    DATABASES['default']['OPTIONS'] = {
+        'sslmode': 'require',
+    }
 
 
 # Password validation
