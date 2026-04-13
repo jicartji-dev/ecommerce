165aa3d6c7c7d92d7d4ef99bef3277ce1c2b0ad3
AdarshFusion <chauhanadarsh432@gmail.com>
2026-04-13 10:50:50 +0530

    utr add

--- cartji/settings.py
+++ cartji/settings.py
@@ -88,28 +88,6 @@ WSGI_APPLICATION = 'cartji.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.environ.get("DATABASE_URL")
-#     )
-# }
-
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get("DATABASE_URL"),
-#         conn_max_age=600,
-#         ssl_require=True,
-#     )
-# }
-
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
