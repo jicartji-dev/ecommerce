a59c3c39e413fe6646105530444b0db3e9c7296c
AdarshFusion <chauhanadarsh432@gmail.com>
2026-04-16 11:59:46 +0530

    fixing order id

--- cartjiapp/utils.py
+++ cartjiapp/utils.py
@@ -1,9 +1,18 @@
-import datetime
+from django.utils.timezone import now
 from .models import Order
 
 def generate_order_id():
-    year = datetime.date.today().year
-    last_order = Order.objects.order_by("-id").first()
+    year = now().year
+
+    # 🔥 get last order based on order_id (NOT id)
+    last_order = Order.objects.filter(
+        order_id__startswith=f"CJ-{year}"
+    ).order_by("-order_id").first()
+
     if last_order:
-        return f"CJ-{year}-{last_order.id + 1:04d}"
-    return f"CJ-{year}-0001"
+        last_number = int(last_order.order_id.split("-")[-1])
+        new_number = last_number + 1
+    else:
+        new_number = 1
+
+    return f"CJ-{year}-{str(new_number).zfill(4)}"
